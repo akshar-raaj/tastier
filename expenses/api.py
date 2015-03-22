749cf3bd88abbf122c79147b4f686065cb5f8363
Akshar Raaj <akshar@agiliq.com>
2015-03-22 12:15:54 +0530

    Possible to POST expense data now

--- expenses/api.py
+++ expenses/api.py
@@ -1,4 +1,5 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 
 from .models import Expense
 
@@ -8,3 +9,8 @@ class ExpenseResource(ModelResource):
         queryset = Expense.objects.all()
         resource_name = 'expense'
         fields = ['description', 'amount']
+        filtering = {
+            'amount': ['gt'],
+            'description': ['icontains']
+        }
+        authorization = Authorization()
